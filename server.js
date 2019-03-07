42ac8845ff04ca50d18e0381c2ec766fa8b91d60
var-che <filo.vlada@gmail.com>
2019-03-07 17:02:20 +0100

    push 2

--- server.js
+++ server.js
@@ -27,6 +27,16 @@ app.use('/users', Users)
 app.use('/teachers', Teachers)
 app.use('/courses', Courses)
 
+// serve statuc assets
+if( process.env.NODE_ENV ==="production" ) {
+    // set static folder
+    app.use( express.static('client/build'))
+
+    app.get( '*', (req, res) => {
+        res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html' ));
+    })
+}
+
 app.listen(port, () => {
     console.log("Server is running on port: " + port)
 })
